788089f7ac0a776e1f9ab4ce4df25a300dd03175
Renee Messersmith <morganrmessersmith@gmail.com>
2024-10-17 19:54:28 -0400

    add poster rendering functionality

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,9 +1,21 @@
 import './MoviesContainer.css';
 
-function Movies() {
+function Movies({posters}) {
+  // const { posters } = props;
+
+  const showPosters = posters.map(poster => {
+    return (
+      <section class="poster">
+        <img key={poster.id} src={poster.poster_path} alt={'movie poster'}/>
+        <div>{poster.vote_count}</div>
+      </section>
+    )
+  })
+
   return (
-      <section className='MoviesContainer'>
-        <p>We'll make some movie posters show up here!</p>
+      <section className='movies-container'>
+        {/* <p>We'll make some movie posters show up here!</p> */}
+        {showPosters}
       </section>
   );
 }
