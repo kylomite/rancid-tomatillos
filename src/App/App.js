7a84fb65ef726db310bad040864f95bf6570834d
Kyle Delaney <khdelaney99@gmail.com>
2024-10-20 15:29:52 -0400

    poorly implement incrementing movie vote

--- src/App/App.js
+++ src/App/App.js
@@ -19,23 +19,76 @@ function App() {
   }, [] );
 
   function changeVoteCountData(id, vote) {
+    // setPosters((prevMoviePosters) => {
+    //   return prevMoviePosters.map((movie) => {
+    //     if (movie.id === id) {
+    //       return {
+    //         ...movie,
+    //         vote_count: vote === "up" ? movie.vote_count + 1 : movie.vote_count - 1
+    //       };
+    //     }
+    //     return movie;
+    //   });
+    // });
     setPosters((prevMoviePosters) => {
       return prevMoviePosters.map((movie) => {
-        if (movie.id === id) {
-          return {
-            ...movie,
-            vote_count: vote === "up" ? movie.vote_count + 1 : movie.vote_count - 1
-          };
+        if (movie.id === id && vote === "up") {
+          fetch(`https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+              id: `${id}`,
+              vote_direction: "up"
+            })
+            .then(response => response.json())
+            .then(specificMovie => setPosters([...posters, specificMovie]))
+            .catch(error => alert(error)) 
+          })
+        } else if(movie.id === id && vote === "down"){
+          fetch(`https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+              id: `${id}`,
+              vote_direction: "down"
+            })
+          })
+          .then(response => response.json())
+          .then(specificMovie => setPosters([...posters, specificMovie]))
+          .catch(error => alert(error))
         }
         return movie;
       });
     });
+    // if(vote === "up"){
+    //   fetch(`https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/${id}`, {
+    //     method: "PATCH",
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify({
+    //       id: `${id}`,
+    //       vote_direction: "up"
+    //     })
+    //   })
+    //   .then(response => response.json())
+    //   .then(specificMovie => console.log(specificMovie));
+    // } else {
+    //   fetch(`https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies/${id}`, {
+    //     method: "PATCH",
+    //     headers: {"Content-Type": "application/json"},
+    //     body: JSON.stringify({
+    //       id: `${id}`,
+    //       vote_direction: "down"
+    //     })
+    //   })
+    //   .then(response => response.json())
+    //   .then(specificMovie => setPosters([...posters, specificMovie]));
+    // }
   }
 
   function showMovieDetails(movie) {
     console.log("showMovieDetails CLICK!")
     // setSelectedMovie(movie);
-    setSelectedMovie(movieDetails);
+    setSelectedMovie(movie);
     // ^ hardcoding Spirited Away movie details
   };
 
