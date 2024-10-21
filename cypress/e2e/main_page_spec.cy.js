165437fec548c1aaac0a4f7a4da47c654f87b2aa
Kyle Delaney <khdelaney99@gmail.com>
2024-10-21 19:42:47 -0400

    add cypress test for main page

---
+++ cypress/e2e/main_page_spec.cy.js
@@ -0,0 +1,22 @@
+describe('main page spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.intercept('GET', 'https://rancid-tomatillos-api-cc6f59111a05.herokuapp.com/api/v1/movies', {
+      statusCode: 200,
+      fixture: 'movie_posters'
+    })
+  })
+  it('shows the main page', () => {
+    cy.get('h1').should('contain', 'rancid tomatillos')
+    .get('.movies-container').should('exist')
+    .get('.poster').should('have.length', 4)
+    .get('.poster').first().find('img').should('exist')
+    .get('.poster').first().find('div [class="vote-count"]').should('exist')
+    .get('.poster').first().find('div [class="upvote-button"]').should('exist')
+    .get('.poster').first().find('div [class="downvote-button"]').should('exist')
+    .get('.poster').last().find('img').should('exist')
+    .get('.poster').last().find('div [class="vote-count"]').should('exist')
+    .get('.poster').last().find('div [class="upvote-button"]').should('exist')
+    .get('.poster').last().find('div [class="downvote-button"]').should('exist')
+  })
+})
